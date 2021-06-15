079e00642eb83c966961d30b8c524ba5a91cffe7
Angel Soto <85462911+TEODOE-LC101@users.noreply.github.com>
2021-06-15 02:31:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let jsCreator = "Brendan Eich";
+
+console.log(jsCreator[-1]);
+console.log(jsCreator[42]);
+console.log("\n")
+console.log(jsCreator[0]);
+console.log(jsCreator[8]);
+console.log("\n")
+console.log("My initials are " + jsCreator[0] + ". " + jsCreator[8] + ".");
+console.log("\n")
+// or Template String (this is easier)
+console.log(`My initials are ${jsCreator[0]}. ${jsCreator[8]}. And this is a template string.`)
