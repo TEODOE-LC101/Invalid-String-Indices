84efd2ec94fd0e1873672feb0f9a17256d044e86
Angel Soto <85462911+TEODOE-LC101@users.noreply.github.com>
2021-06-15 02:49:01 +0000

    I added check your understanding.

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ console.log("My initials are " + jsCreator[0] + ". " + jsCreator[8] + ".");
 console.log("\n")
 // or Template String (this is easier)
 console.log(`My initials are ${jsCreator[0]}. ${jsCreator[8]}. And this is a template string.`)
+console.log("\n")
+/*
+let phrase = "Code for fun"
+console.log(phrase[2]);
+*/
+console.log("\n")
+let myStr = "Index";
+console.log(Boolean(myStr[0]));
+console.log("\n")
+let phrase = "JavaScript rocks!";
+console.log(phrase[phrase.length - 8]); // T
