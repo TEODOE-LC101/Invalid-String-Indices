a88c3aca75ac6c78156303670573eaede2cc91fb
Angel Soto <85462911+TEODOE-LC101@users.noreply.github.com>
2021-06-15 04:59:47 +0000

    Used .toLowerCase method.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 let jsCreator = "Brendan Eich";
 
 console.log(jsCreator[-1]);
@@ -6,18 +7,32 @@ console.log("\n")
 console.log(jsCreator[0]);
 console.log(jsCreator[8]);
 console.log("\n")
-console.log("My initials are " + jsCreator[0] + ". " + jsCreator[8] + ".");
+console.log("My initials are " + jsCreator[0] + ". " + jsCreator[8] + ".");*/
 console.log("\n")
 // or Template String (this is easier)
+/*
 console.log(`My initials are ${jsCreator[0]}. ${jsCreator[8]}. And this is a template string.`)
+*/
 console.log("\n")
 /*
 let phrase = "Code for fun"
 console.log(phrase[2]);
 */
 console.log("\n")
+/*
 let myStr = "Index";
 console.log(Boolean(myStr[0]));
 console.log("\n")
 let phrase = "JavaScript rocks!";
 console.log(phrase[phrase.length - 8]); // T
+*/
+
+let nonprofit = "LaunchCode";
+
+console.log(nonprofit.toLowerCase());
+console.log(nonprofit);
+
+let teo = "TEODOE";
+
+console.log(teo.toLowerCase());
+console.log(teo);
